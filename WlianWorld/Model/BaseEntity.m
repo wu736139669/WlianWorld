5462ae4e292832664d0696bf2abd30725a8c062d
wushenghua <wushenghua@linggan.com>
2016-01-20 14:50:01 +0800

    删除小鱼的信息

--- WlianWorld/Model/BaseEntity.m
+++ WlianWorld/Model/BaseEntity.m
@@ -6,29 +6,28 @@
 //  Copyright © 2015年 ash. All rights reserved.
 //
 
+#import "AWordDefault.h"
 #import "BaseEntity.h"
 #import "BaseModel.h"
 #import "NSString+HXAddtions.h"
-#import "AWordDefault.h"
 @implementation proFile
 
 @end
 static NSString* app_key = @"123456";
 static NSString* app_key_secret = @"z*~#@kR^*N";
 @implementation BaseEntity
-- (NSDictionary *)encodePro
+- (NSDictionary*)encodePro
 {
-    
-    NSMutableDictionary* dic= [NSMutableDictionary dictionary];;
-    
-    
+
+    NSMutableDictionary* dic = [NSMutableDictionary dictionary];
+    ;
+
     [dic setObject:_callback forKey:@"callback"];
-    if(_pro)
-    {
+    if (_pro) {
         [dic setObject:_pro forKey:@"params"];
     }
     [dic setObject:@"1" forKey:@"devicecode"];
-    
+
     NSString* uid = [AWordDefault getUid];
     if (uid) {
         [dic setObject:uid forKey:@"uid"];
@@ -38,63 +37,7 @@ static NSString* app_key_secret = @"z*~#@kR^*N";
     if (token) {
         [dic setObject:token forKey:@"token"];
     }
-    NSString* shopid = [AWordDefault getShopId];
-    if (shopid) {
-        [dic setObject:shopid forKey:@"shopid"];
-    }
-    NSTimeInterval timeStamp = [[NSDate date] timeIntervalSince1970];
-    [dic setObject:[NSString stringWithFormat:@"%f", timeStamp] forKey:@"timestamp"];
-    
-    [dic setObject:[self getSignWithDic:dic] forKey:@"sign"];
-    
-    
-    DLog(@"参数是:%@",dic);
-    NSString* jsonKey = [NSString jsonStringWithDictionary:(NSDictionary*)dic];
-    NSData *nsdata = [jsonKey
-                      dataUsingEncoding:NSUTF8StringEncoding];
-    NSString *baseKey = [nsdata base64EncodedStringWithOptions:0];
-    NSMutableString *key = [[NSMutableString alloc]initWithString:baseKey];
-    [key stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    NSDictionary *resultDic = @{@"key":key};
-    return resultDic;
+    return dic;
 }
 
-
--(NSString*)getSignWithDic:(NSDictionary*)dic
-{
-    NSString* resultStr = [self getStrWithDic:dic];
-    NSString* appkey = [NSString stringWithFormat:@"%@%@",app_key,app_key_secret];
-    NSString* md5Appkey = [[appkey md5] substringWithRange:NSMakeRange(8, 18)];
-    resultStr = [resultStr stringByAppendingString:md5Appkey];
-    resultStr = [resultStr md5];
-    return resultStr;
-}
-
--(NSString*)getStrWithDic:(NSDictionary*)dic
-{
-    NSString* resultStr = @"";
-    
-    for (NSString* key in [dic allKeys]) {
-        NSObject* obj = [dic objectForKey:key];
-        if ([obj isKindOfClass:[NSDictionary class]]) {
-            obj = [self getStrWithDic:(NSDictionary*)obj];
-            if ([resultStr isEqualToString:@""]) {
-                resultStr = obj;
-                
-            }else{
-                resultStr = [NSString stringWithFormat:@"%@&%@",resultStr,obj];
-                
-            }        }else{
-            if ([resultStr isEqualToString:@""]) {
-                resultStr = [NSString stringWithFormat:@"%@=%@",key,obj];
-                
-            }else{
-                resultStr = [NSString stringWithFormat:@"%@&%@=%@",resultStr,key,obj];
-                
-            }
-        }
-        
-    }
-    return resultStr;
-}
 @end
